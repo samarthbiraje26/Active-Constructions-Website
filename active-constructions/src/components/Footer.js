4532e17287a53c3b2a22410037c07fc934587403
samarthbiraje26 <samarthbiraje@gmail.com>
2025-08-31 12:10:01 +0530

    Update Footer.js

---
+++ active-constructions/src/components/Footer.js
@@ -0,0 +1,8 @@
+function Footer() {
+  return (
+    <footer className="bg-gray-800 text-white text-center p-4 mt-6">
+      <p>© {new Date().getFullYear()} Active Constructions. All rights reserved.</p>
+    </footer>
+  );
+}
+export default Footer;
