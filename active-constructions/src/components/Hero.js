edabe27a81972bc6ed7a874f8c52b395d81da1fd
samarthbiraje26 <samarthbiraje@gmail.com>
2025-08-31 12:06:29 +0530

    Update Hero.js

---
+++ active-constructions/src/components/Hero.js
@@ -0,0 +1,9 @@
+function Hero() {
+  return (
+    <section className="bg-blue-100 p-10 text-center">
+      <h2 className="text-3xl font-bold">Building Dreams, Creating Reality</h2>
+      <p className="mt-2">Trusted construction company in Pune & Mumbai.</p>
+    </section>
+  );
+}
+export default Hero;
