93e4db8d61951ad950f05f795a183c5aa011382b
samarthbiraje26 <samarthbiraje@gmail.com>
2025-08-31 12:04:49 +0530

    Update App.js

--- active-constructions/src/App.js
+++ active-constructions/src/App.js
@@ -1,10 +1,29 @@
 import Header from "./components/Header";
+import Hero from "./components/Hero";
+import Services from "./components/Services";
+import Projects from "./components/Projects";
+import NewsSection from "./components/NewsSection";
+import Contact from "./components/Contact";
+import Footer from "./components/Footer";
 
 function App() {
+  // Example project data (props drilling)
+  const projectData = [
+    { name: "Skyline Towers", location: "Pune" },
+    { name: "Green Valley", location: "Mumbai" }
+  ];
+
   return (
     <>
       <Header />
-      {/* Other sections */}
+      <Hero />
+      <Services />
+      <Projects projects={projectData} />
+      <NewsSection />
+      <Contact />
+      <Footer />
     </>
   );
 }
+
+export default App;
